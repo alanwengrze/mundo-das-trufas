e902c4f55d4c161b01ed8248bf8c07c859344cd0
alanwengrze <alanwengrzee@gmail.com>
2025-02-14 05:06:32 -0300

    :wrench: config: url changed to prod

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "sonner";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://mundo-das-trufas.vercel.app/api",
 });
 
 api.interceptors.response.use(
