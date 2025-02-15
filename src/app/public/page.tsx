0c6312838670c4bf0808a8d486a3f0fb079773ea
alanwengrze <alanwengrzee@gmail.com>
2025-02-15 08:54:53 -0300

    :bug: fix: remove code without use

--- src/app/public/page.tsx
+++ src/app/public/page.tsx
@@ -4,11 +4,11 @@ import { ProductCard } from "@/components/product-card"
 import { useProduct } from "@/contexts/product-context"
 export default function Home() {
   const { products } = useProduct()
-  const bolos = products.filter((product) => product.category === "bolos")
+  // const bolos = products.filter((product) => product.category === "bolos")
 
   return (
     <div className=" grid grid-flow-dense gap-4 grid-cols-1 sm:grid-cols-3 ">
-      
+
       {
         products.map((product) => (
           <ProductCard 
--- src/auth.config.ts
+++ src/auth.config.ts
@@ -9,16 +9,6 @@ export default {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }),
-    // EmailProvider({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASS
-    //     }
-    //   },
-    // }),
     Mailgun({
       apiKey: process.env.MAILGUN_API_KEY
     })
